b3f95030d29dda8cd39227289b750827ac419597
LazyDoge <lazywoolf@gmail.com>
2019-10-18 21:00:44 +0300

    clickhandler for boxes

--- app/src/main/java/com/sky/colormyviews/MainActivity.kt
+++ app/src/main/java/com/sky/colormyviews/MainActivity.kt
@@ -1,12 +1,38 @@
 package com.sky.colormyviews
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setListeners()
+    }
+
+    private fun setListeners() {
+        val clickableViews: List<View> =
+            listOf(box_one_text, box_two_text, box_three_text, box_four_text, box_five_text,
+                constraint_layout)
+        for (clickableView in clickableViews) {
+            clickableView.setOnClickListener { makeColor(it) }
+        }
+    }
+
+    private fun makeColor(view: View) {
+        when (view.id) {
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+
+            R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
+            R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
     }
 }
